ed8402b6233f9086122a654cfda370be0a78d80b
L.Y <liu2010y@163.com>
2016-12-06 23:04:41 +0800

    Initial commit

---
+++ src/com/datarestore/Constants.java
@@ -0,0 +1,13 @@
+package com.datarestore;
+
+/**
+ * Created by LiuY on 2016/12/6.
+ */
+public class Constants {
+    public static String URL = "";
+
+    public static void main(String[] args){
+        System.out.print("dddd");
+
+    }
+}
